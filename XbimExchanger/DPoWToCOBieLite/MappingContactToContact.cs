a9db473e4ec9d550e13c1ca7cd3cda2208173edc
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-01-25 18:13:29 +0100

    DPoW to COBieLite contact object creation changed to create ID

--- XbimExchanger/DPoWToCOBieLite/MappingContactToContact.cs
+++ XbimExchanger/DPoWToCOBieLite/MappingContactToContact.cs
@@ -10,6 +10,12 @@ namespace XbimExchanger.DPoWToCOBieLite
 {
     public class MappingContactToContact : DPoWToCOBieLiteMapping<Contact, ContactType>
     {
+        public override ContactType CreateTargetObject()
+        {
+            var obj = base.CreateTargetObject();
+            obj.externalID = Exchanger.GetStringIdentifier();
+            return obj;
+        }
         protected override ContactType Mapping(Contact source, ContactType target)
         {
             throw new NotImplementedException();
