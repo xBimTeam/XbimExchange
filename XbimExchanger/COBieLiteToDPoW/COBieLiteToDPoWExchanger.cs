f5472d37403599cac1842040df667dbb4256fba9
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-01-28 15:55:36 +0100

    Implementation of COBieLite to DPoW conversion started.

---
+++ XbimExchanger/COBieLiteToDPoW/COBieLiteToDPoWExchanger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.COBieLite;
+using Xbim.DPoW.Interfaces;
+
+namespace XbimExchanger.COBieLiteToDPoW
+{
+    public class COBieLiteToDPoWExchanger : XbimExchanger<FacilityType, PlanOfWork>
+    {
+        /// <summary>
+        /// Constructor of the exchanger class for conversion from COBieLite to Digital Plan of Work.
+        /// </summary>
+        /// <param name="facility">Input facility (root object of COBieLite data model)</param>
+        /// <param name="plan">Output DPoW model - this is supposed to be an empty model.</param>
+        public COBieLiteToDPoWExchanger(FacilityType facility, PlanOfWork plan) : base(facility, plan)
+        {
+
+        }
+
+        /// <summary>
+        /// Converts COBieLite model defined in constructor into DPoW.
+        /// </summary>
+        /// <returns>Digital Plan of Work</returns>
+        public override PlanOfWork Convert()
+        {
+            var map = GetOrCreateMappings<MappingFacilityTypeToPlanOfWork>();
+            return map.AddMapping(SourceRepository, TargetRepository);
+        }
+    }
+}
