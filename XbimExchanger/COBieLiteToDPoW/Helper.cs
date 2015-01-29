33de458242926c52fd53b5e699780631dfa75408
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-01-29 16:05:13 +0100

    COBieLite to DPoW conversion in progress.

---
+++ XbimExchanger/COBieLiteToDPoW/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XbimExchanger.COBieLiteToDPoW
+{
+    static class Helper
+    {
+        public static TOut TryConvertEnum<TIn, TOut>(TIn input, TOut defaultValue) where TIn : struct where TOut: struct
+        {
+            TOut output;
+            if (Enum.TryParse<TOut>(input.ToString(), out output))
+                return output;
+            return defaultValue;
+        }
+    }
+}
