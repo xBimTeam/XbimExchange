76a50bf704341b167be45c3bc6b21f083395a5b3
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-01-30 16:49:06 +0100

    COBieLiteUK proposal

---
+++ Tests/COBieLiteUKTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.COBieLiteUK;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Tests
+{
+    [TestClass]
+    public class COBieLiteUKTests
+    {
+        [TestMethod]
+        public void COBieLiteUKCreation()
+        {
+            var model = new FacilityStageCollectionType();
+            model.FacilityStage = new List<FacilityStageType>();
+            model.FacilityStage.AddRange(new []{
+                new FacilityStageType(){
+                    externalID = Guid.NewGuid().ToString(),
+                    FacilityStageCode = "A4",
+                    FacilityStageDescription = "Stage A4",
+                    FacilityStageName = "Preliminary design",
+                    FacilityStageStartDate = DateTime.Now,
+                    FacilityStageEndDate = DateTime.Now.AddDays(28),
+                    FacilityStageAttributes = new AttributeCollectionType(){
+                        Attribute = new List<AttributeType>(new []{
+                            new AttributeType1(){
+                                AttributeName = "New attribute 1",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){Item = new AttributeStringValueType(){StringValue = "Value AAA"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue}
+                            },
+                            new AttributeType1(){
+                                AttributeName = "New attribute 2",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){Item = new AttributeStringValueType(){StringValue = "Value BBB"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue}
+                            }
+                        })
+                    },
+                    Facility = new FacilityType1(){
+                        externalID = Guid.NewGuid().ToString(),
+                        FacilityDefaultAreaUnit = AreaUnitSimpleType.squaremeters,
+                        FacilityDefaultAreaUnitSpecified = true,
+                        FacilityDefaultCurrencyUnit = CurrencyUnitSimpleType.BritishPounds,
+                        FacilityDefaultCurrencyUnitSpecified = true,
+                        FacilityDefaultLinearUnit = LinearUnitSimpleType.millimeters,
+                        FacilityDefaultLinearUnitSpecified = true,
+                        FacilityDefaultMeasurementStandard = "NRM",
+                        FacilityDefaultVolumeUnit = VolumeUnitSimpleType.cubicmeters,
+                        FacilityDefaultVolumeUnitSpecified = true,
+                        FacilityDeliverablePhaseName = "Phase A",
+                        FacilityDescription = "New facility description",
+                        FacilityName = "Ellison Building",
+                        ProjectAssignment = new ProjectType(){externalID = Guid.NewGuid().ToString(), ProjectName = "Project A"},
+                        SiteAssignment = new SiteType(){externalID = Guid.NewGuid().ToString(), SiteName = "Site A"},
+                        Zones = new ZoneCollectionType(){Zone = new List<ZoneType>(new []{
+                            new ZoneType1(){
+                                externalID = Guid.NewGuid().ToString(),
+                                ZoneName = "Zone A",
+                                ZoneCategory = "45.789.78",
+                                ZoneDescription = "Description of the zone A"
+                            }
+                        })}
+                    }
+                },
+                new FacilityStageType(){
+                    externalID = Guid.NewGuid().ToString(),
+                    FacilityStageCode = "A4",
+                    FacilityStageDescription = "Stage A4",
+                    FacilityStageName = "Preliminary design",
+                    FacilityStageStartDate = DateTime.Now,
+                    FacilityStageEndDate = DateTime.Now.AddDays(28),
+                    FacilityStageAttributes = new AttributeCollectionType(){
+                        Attribute = new List<AttributeType>(new []{
+                            new AttributeType1(){
+                                AttributeName = "New attribute 1",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){Item = new AttributeStringValueType(){StringValue = "Value AAA"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue}
+                            },
+                            new AttributeType1(){
+                                AttributeName = "New attribute 2",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){Item = new AttributeStringValueType(){StringValue = "Value BBB"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue}
+                            }
+                        })
+                    },
+                    Facility = new FacilityType1(){
+                        externalID = Guid.NewGuid().ToString(),
+                        FacilityDefaultAreaUnit = AreaUnitSimpleType.squaremeters,
+                        FacilityDefaultAreaUnitSpecified = true,
+                        FacilityDefaultCurrencyUnit = CurrencyUnitSimpleType.BritishPounds,
+                        FacilityDefaultCurrencyUnitSpecified = true,
+                        FacilityDefaultLinearUnit = LinearUnitSimpleType.millimeters,
+                        FacilityDefaultLinearUnitSpecified = true,
+                        FacilityDefaultMeasurementStandard = "NRM",
+                        FacilityDefaultVolumeUnit = VolumeUnitSimpleType.cubicmeters,
+                        FacilityDefaultVolumeUnitSpecified = true,
+                        FacilityDeliverablePhaseName = "Phase A",
+                        FacilityDescription = "New facility description",
+                        FacilityName = "Ellison Building",
+                        ProjectAssignment = new ProjectType(){externalID = Guid.NewGuid().ToString(), ProjectName = "Project A"},
+                        SiteAssignment = new SiteType(){externalID = Guid.NewGuid().ToString(), SiteName = "Site A"},
+                        Zones = new ZoneCollectionType(){Zone = new List<ZoneType>(new []{
+                            new ZoneType1(){
+                                externalID = Guid.NewGuid().ToString(),
+                                ZoneName = "Zone A",
+                                ZoneCategory = "45.789.78",
+                                ZoneDescription = "Description of the zone A"
+                            }
+                        })}
+                    }
+                },
+                new FacilityStageType(){
+                    externalID = Guid.NewGuid().ToString(),
+                    FacilityStageCode = "A4",
+                    FacilityStageDescription = "Stage A4",
+                    FacilityStageName = "Preliminary design",
+                    FacilityStageStartDate = DateTime.Now,
+                    FacilityStageEndDate = DateTime.Now.AddDays(28),
+                    FacilityStageAttributes = new AttributeCollectionType(){
+                        Attribute = new List<AttributeType>(new []{
+                            new AttributeType1(){
+                                AttributeName = "New attribute 1",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){
+                                    Item = new AttributeStringValueType(){StringValue = "Value AAA"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue
+                                }
+                            },
+                            new AttributeType1(){
+                                AttributeName = "New attribute 2",
+                                externalID = Guid.NewGuid().ToString(),
+                                AttributeCategory = "Requirement",
+                                AttributeValue = new AttributeValueType(){
+                                    Item = new AttributeStringValueType(){StringValue = "Value BBB"},
+                                    ItemElementName = ItemChoiceType.AttributeStringValue
+                                }
+                            }
+                        })
+                    },
+                    Facility = new FacilityType1(){
+                        externalID = Guid.NewGuid().ToString(),
+                        FacilityDefaultAreaUnit = AreaUnitSimpleType.squaremeters,
+                        FacilityDefaultAreaUnitSpecified = true,
+                        FacilityDefaultCurrencyUnit = CurrencyUnitSimpleType.BritishPounds,
+                        FacilityDefaultCurrencyUnitSpecified = true,
+                        FacilityDefaultLinearUnit = LinearUnitSimpleType.millimeters,
+                        FacilityDefaultLinearUnitSpecified = true,
+                        FacilityDefaultMeasurementStandard = "NRM",
+                        FacilityDefaultVolumeUnit = VolumeUnitSimpleType.cubicmeters,
+                        FacilityDefaultVolumeUnitSpecified = true,
+                        FacilityDeliverablePhaseName = "Phase A",
+                        FacilityDescription = "New facility description",
+                        FacilityName = "Ellison Building",
+                        ProjectAssignment = new ProjectType(){externalID = Guid.NewGuid().ToString(), ProjectName = "Project A"},
+                        SiteAssignment = new SiteType(){externalID = Guid.NewGuid().ToString(), SiteName = "Site A"},
+                        Zones = new ZoneCollectionType(){Zone = new List<ZoneType>(new []{
+                            new ZoneType1(){
+                                externalID = Guid.NewGuid().ToString(),
+                                ZoneName = "Zone A",
+                                ZoneCategory = "45.789.78",
+                                ZoneDescription = "Description of the zone A"
+                            }
+                        })}
+                    }
+                }
+            });
+            using (var file = File.Create("test.xml"))
+            {
+                model.Save(file);
+                file.Close();                
+            }
+        }
+    }
+}
---
+++ Xbim.COBieLiteUK/FacilityStageCollectionType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Xbim.COBieLiteUK
+{
+    public partial class FacilityStageCollectionType
+    {
+        public void Save(Stream stream)
+        {
+            var serializer = new XmlSerializer(this.GetType());
+            serializer.Serialize(stream, this, 
+                new XmlSerializerNamespaces(new []{
+                    new XmlQualifiedName("cobielite", "http://docs.buildingsmartalliance.org/nbims03/cobie/cobielite"),
+                    new XmlQualifiedName("core", "http://docs.buildingsmartalliance.org/nbims03/cobie/core"),
+                }));
+        }
+    }
+}
