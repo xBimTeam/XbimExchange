6b4d16747bdeb9dabafb12e1991afa3cc03c891f
SteveLockley <steve.lockley@northumbria.ac.uk>
2015-02-25 17:11:31 +0000

    Attribute valuetype to Ifc conversion amended

---
+++ XbimExchanger/COBieLiteHelpers/AttributeValueTypeExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Xbim.COBieLite;
+
+namespace XbimExchanger.COBieLiteHelpers
+{
+    public static class AttributeValueTypeExtensions
+    {
+
+        public static XbimSimplePropertyType SimplePropertyType(this AttributeValueType valueType)
+        {
+            if (valueType.Item == null) return XbimSimplePropertyType.Null;
+            string valueTypeString = valueType.Item.GetType().Name;
+            
+            switch (valueTypeString)
+            {
+                case "BooleanValueType":
+                    return XbimSimplePropertyType.SimpleBoolean;
+                case "DateTime":
+                    return XbimSimplePropertyType.SimpleDateTime;
+                case "AttributeDecimalValueType":
+                    var dt = valueType.Item as AttributeDecimalValueType;
+                    return dt != null && (dt.MaxValueDecimalSpecified || dt.MinValueDecimalSpecified)
+                    ? XbimSimplePropertyType.BoundedDecimal
+                    : XbimSimplePropertyType.SimpleDecimal;
+                case "AttributeIntegerValueType":
+                    var it = valueType.Item as AttributeIntegerValueType;
+                    return it != null && (it.MaxValueInteger - it.MinValueInteger > 0)
+                    ? XbimSimplePropertyType.BoundedInteger
+                    : XbimSimplePropertyType.SimpleInteger;
+                case "AttributeMonetaryValueType":
+                    return XbimSimplePropertyType.SimpleMonetary;
+                case "AttributeStringValueType":
+                    var st = valueType.Item as AttributeStringValueType;
+                    return st != null && st.AllowedValues.Any()
+                    ? XbimSimplePropertyType.EnumerationString
+                    : XbimSimplePropertyType.SimpleString;
+                default:
+                    return XbimSimplePropertyType.Null;
+            }
+        }   
+    }
+}
