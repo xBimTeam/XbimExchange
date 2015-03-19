41fb802fedb24170c288cb3432a3684613d59921
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-03-19 22:37:08 +0100

    Project and Site objects created within root Facility object to keep information about project and site which are blended into Facility in COBie. COBie Coordinate implemented as a Representation object which can be assigned to any COBieObject.

--- Tests/COBieLiteUKTests.cs
+++ Tests/COBieLiteUKTests.cs
@@ -17,6 +17,9 @@ namespace Tests
         {
             var facility = new Facility
             {
+                CreatedOn = DateTime.Now,
+                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                Category = "Schools",
                 ExternalId = System.Guid.NewGuid().ToString(),
                 AreaUnits = AreaUnit.squaremeters,
                 CurrencyUnit = CurrencyUnit.GBP,
@@ -26,14 +29,22 @@ namespace Tests
                 Phase = "Phase A",
                 Description = "New facility description",
                 Name = "Ellison Building",
-                ExternalProjectIdentifier = Guid.NewGuid().ToString(),
-                ProjectName = "Project A",
-                ExternalSiteIdentifier = Guid.NewGuid().ToString(),
-                SiteName = "Site A",
+                Project = new Project
+                {
+                    ExternalId = Guid.NewGuid().ToString(),
+                    Name = "Project A"
+                },
+                Site = new Site
+                {
+                    ExternalId = Guid.NewGuid().ToString(),
+                    Name = "Site A"
+                },
                 Zones = new List<Zone>
                 {
                     new Zone
                     {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
                         ExternalId = Guid.NewGuid().ToString(),
                         Name = "Zone A",
                         Category = "45.789.78",
@@ -48,64 +59,201 @@ namespace Tests
                 },
                 Contacts = new List<Contact>
                 {
-                    new Contact{CreatedOn = DateTime.Now, Category = "12.45.56", FamilyName = "Martin", Email = "martin.cerny@northumbria.ac.uk", GivenName = "Cerny"},
-                    new Contact{CreatedOn = DateTime.Now, Category = "12.45.56", FamilyName = "Peter", Email = "peter.pan@northumbria.ac.uk", GivenName = "Pan"},
-                    new Contact{CreatedOn = DateTime.Now, Category = "12.45.56", FamilyName = "Paul", Email = "paul.mccartney@northumbria.ac.uk", GivenName = "McCartney"}
-                },
-                Floors = new List<Floor> { new Floor
-                {
-                    CreatedOn = DateTime.Now,
-                    Elevation = 15000,
-                    Height = 3400,
-                    Spaces = new List<Space>
+                    new Contact
                     {
-                        new Space
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Category = "12.45.56",
+                        FamilyName = "Martin",
+                        Email = "martin.cerny@northumbria.ac.uk",
+                        GivenName = "Cerny"
+                    },
+                    new Contact
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Category = "12.45.56",
+                        FamilyName = "Peter",
+                        Email = "peter.pan@northumbria.ac.uk",
+                        GivenName = "Pan"
+                    },
+                    new Contact
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Category = "12.45.56",
+                        FamilyName = "Paul",
+                        Email = "paul.mccartney@northumbria.ac.uk",
+                        GivenName = "McCartney"
+                    }
+                },
+                Floors = new List<Floor>
+                {
+                    new Floor
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Elevation = 15000,
+                        Height = 3400,
+                        Spaces = new List<Space>
                         {
-                            CreatedOn = DateTime.Now,
-                            Category = "Sp_02_78_98",
-                            Description = "First front room in COBieLiteUK ever",
-                            Name = "A001 - Front Room"
-                        },
-                        new Space
-                        {
-                            CreatedOn = DateTime.Now,
-                            Category = "Sp_02_78_98",
-                            Description = "First living room in COBieLiteUK ever",
-                            Name = "A002 - Living Room"
-                        },
-                        new Space
-                        {
-                            CreatedOn = DateTime.Now,
-                            Category = "Sp_02_78_98",
-                            Description = "First bedroom in COBieLiteUK ever",
-                            Name = "A003 - Bedroom"
+                            new Space
+                            {
+                                CreatedOn = DateTime.Now,
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                Category = "Sp_02_78_98",
+                                Description = "First front room in COBieLiteUK ever",
+                                Name = "A001 - Front Room",
+                                UsableHeight = 3500,
+                                NetArea = 6
+                            },
+                            new Space
+                            {
+                                CreatedOn = DateTime.Now,
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                Category = "Sp_02_78_98",
+                                Description = "First living room in COBieLiteUK ever",
+                                Name = "A002 - Living Room",
+                                UsableHeight = 4200,
+                                NetArea = 55
+                            },
+                            new Space
+                            {
+                                CreatedOn = DateTime.Now,
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                Category = "Sp_02_78_98",
+                                Description = "First bedroom in COBieLiteUK ever",
+                                Name = "A003 - Bedroom",
+                                UsableHeight = 4100,
+                                NetArea = 25
+                            }
                         }
                     }
-                } },
+                },
                 AssetTypes = new List<AssetType>
                 {
                     new AssetType
                     {
                         CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
                         Name = "Brick layered wall",
                         Assets = new List<Asset>
                         {
-                            new Asset{CreatedOn = DateTime.Now, Name = "120mm partition wall"},
-                            new Asset{CreatedOn = DateTime.Now, Name = "180mm partition wall"},
-                            new Asset{CreatedOn = DateTime.Now, Name = "350mm external brick wall"}
+                            new Asset
+                            {
+                                CreatedOn = DateTime.Now,
+                                Name = "120mm partition wall",
+                                Representations = new List<Representation>
+                                {
+                                    new Representation
+                                    {
+                                        CreatedOn = DateTime.Now,
+                                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                        X = 0,
+                                        Y = 0,
+                                        Z = 0,
+                                        SizeX = 1000,
+                                        SizeY = 2000,
+                                        SizeZ = 200,
+                                        Name = Guid.NewGuid().ToString()
+                                    }
+                                },
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"}
+                            },
+                            new Asset
+                            {
+                                CreatedOn = DateTime.Now,
+                                Name = "180mm partition wall",
+                                Representations = new List<Representation>
+                                {
+                                    new Representation
+                                    {
+                                        CreatedOn = DateTime.Now,
+                                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                        X = 0,
+                                        Y = 0,
+                                        Z = 0,
+                                        SizeX = 1000,
+                                        SizeY = 2000,
+                                        SizeZ = 200,
+                                        Name = Guid.NewGuid().ToString()
+                                    }
+                                },
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"}
+                            },
+                            new Asset
+                            {
+                                CreatedOn = DateTime.Now,
+                                Name = "350mm external brick wall",
+                                Representations = new List<Representation>
+                                {
+                                    new Representation
+                                    {
+                                        CreatedOn = DateTime.Now,
+                                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                                        X = 0,
+                                        Y = 0,
+                                        Z = 0,
+                                        SizeX = 1000,
+                                        SizeY = 2000,
+                                        SizeZ = 200,
+                                        Name = Guid.NewGuid().ToString()
+                                    }
+                                },
+                                CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"}
+                            }
                         }
                     }
                 },
                 Attributes = new List<Attribute>
                 {
-                    new Attribute{Name = "String attribute", Value = new StringAttributeValue{Value = "Almukantarant"}},
-                    new Attribute{Name = "Boolean attribute", Value = new BooleanAttributeValue{Value = true}},
-                    new Attribute{Name = "Datetime attribute", Value = new DateTimeAttributeValue{Value = DateTime.Now}},
-                    new Attribute{Name = "Decimal attribute", Value = new DecimalAttributeValue{Value = 256.2}},
-                    new Attribute{Name = "Integer attribute", Value = new IntegerAttributeValue{Value = 7}},
-                    new Attribute{Name = "Null attribute"}
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "String attribute",
+                        Value = new StringAttributeValue {Value = "Almukantarant"},
+                        Category = "Submited"
+                    },
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "Boolean attribute",
+                        Value = new BooleanAttributeValue {Value = true},
+                        Category = "Submited"
+                    },
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "Datetime attribute",
+                        Value = new DateTimeAttributeValue {Value = DateTime.Now},
+                        Category = "Submited"
+                    },
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "Decimal attribute",
+                        Value = new DecimalAttributeValue {Value = 256.2},
+                        Category = "Submited"
+                    },
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "Integer attribute",
+                        Value = new IntegerAttributeValue {Value = 7},
+                        Category = "Submited"
+                    },
+                    new Attribute
+                    {
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = new ContactKey {Email = "martin.cerny@northumbria.ac.uk"},
+                        Name = "Null attribute"
+                    }
                 }
-
             };
 
             //save model to file to check it
@@ -116,9 +264,6 @@ namespace Tests
 
             var facility2 = Facility.ReadXml(xmlFile);
             var facility3 = Facility.ReadJson(jsonFile);
-
-
         }
-
     }
 }
