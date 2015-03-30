ac4cec5d93ec1b3981fe5225f0ea40a0c0ee675c
SteveLockley <steve.lockley@northumbria.ac.uk>
2015-03-30 11:24:52 +0100

    Validation Report initial support added. Exchange update to support NBL property sets better

---
+++ Tests/ConvertCobieLiteUKToValidationReportTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPOI.HSSF.UserModel;
+using Xbim.COBieLiteUK;
+using Xbim.CobieLiteUK.Validation;
+
+namespace Tests
+{
+     [DeploymentItem(@"TestFiles\")]
+    [TestClass]
+    public class ConvertCobieLiteUkToValidationReportTests
+    {
+        [TestMethod]
+        public void ConvertCobieLiteUkToValidationReportTest()
+        {
+            var facility = Facility.ReadJson("ValidationReport.json");
+            var validationReport = new XbimValidationReport();
+            Assert.IsTrue(validationReport.Create(facility, "ValidationReport",XbimValidationReport.SpreadSheetFormat.Xlsx ));
+        }
+    }
+}
