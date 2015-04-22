6c428ec53466986d0c5ac0ae1f6237c4b14a1c9c
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-04-22 10:30:12 +0100

    Null and empty list checks in COBieLite.Validate.

--- Tests/CobieLiteUKValidationTests.cs
+++ Tests/CobieLiteUKValidationTests.cs
@@ -76,6 +76,25 @@ namespace Tests
         }
 
         [TestMethod]
+        public void ValidateXlsLakeside2()
+        {
+            const string xlsx = @"c:\Users\mxfm2\Dropbox\Martin\Lakeside_Restaurant_fabric_only.xlsx";
+            string msg;
+            var cobie = Facility.ReadCobie(xlsx, out msg);
+            var req = Facility.ReadJson(@"Lakeside_Restaurant-stage6-COBie.json");
+            var validator = new FacilityValidator();
+            var result = validator.Validate(req, cobie);
+            
+            //create report
+            using (var stream = File.Create(@"c:\Users\mxfm2\Dropbox\Martin\Lakeside_Restaurant_fabric_only.report.xlsx"))
+            {
+                var report = new ExcelValidationReport();
+                report.Create(result, stream, ExcelValidationReport.SpreadSheetFormat.Xlsx);
+                stream.Close();
+            }
+        }
+
+        [TestMethod]
         public void ValidateXlsLakesideForStage0()
         {
             var result = LakeSide0();
