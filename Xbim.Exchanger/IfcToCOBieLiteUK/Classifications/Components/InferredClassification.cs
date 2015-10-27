dea048d599f42d62f12cab0422aef7003f01348f
gcoulby <gcoulby@googlemail.com>
2015-10-27 12:10:04 +0000

    Added classification mapping classes

---
+++ Xbim.Exchanger/IfcToCOBieLiteUK/Classifications/Components/InferredClassification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim.Exchanger.IfcToCOBieLiteUK.Classifications.Components
+{
+    public struct InferredClassification
+    {
+        public string UNICode;
+        public string UNIDescription;
+        public string NBSCode;
+        public string NBSDescription;
+        public string NRMCode;
+        public string NRMDescription;
+    }
+}
---
+++ Xbim.Exchanger/IfcToCOBieLiteUK/Classifications/Components/RegexPatterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim.Exchanger.IfcToCOBieLiteUK.Classifications.Components
+{
+    public static class RegexPatterns
+    {
+        public static string CSVpattern = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"; // Splits CSV files by commas, unless the comma is inside quotes
+        public static string UNIPattern = "[A-z][a-z]_[0-9]*"; // Used to find Uniclass patterns (i.e. Ss_75_50_28_97)
+        public static string NBSPattern = @"[0-9]*-[0-9]*-[0-9]*\/[0-9]*"; // Used to find NBS Patterns (i.e. 75-65-95/110)
+        public static string NRMPattern = @"[0-9]*\.[0-9]*\.[0-9]*"; // Used to find NRM Patterns (i.e. 5.12.1)
+    }
+}
